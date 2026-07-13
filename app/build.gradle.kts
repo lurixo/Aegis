5cff683e36dad724a02acb55f8a83e86684647eb
lurixo <120077386+lurixo@users.noreply.github.com>
2026-07-13 11:31:54 +0900

    Prepare v0.1.0-beta.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.aegis.ime"
         minSdk = 34
         targetSdk = 37
-        versionName = "0.1.0-beta.1"
-        versionCode = 61
+        versionName = "0.1.0-beta.2"
+        versionCode = 62
         ndk { abiFilters += "arm64-v8a" }
     }
 
